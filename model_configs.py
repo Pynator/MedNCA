f36aff9e5c2b6263c9c8f65ea108c0610025949e
Lukas Maninger <Lukas.Maninger@stud.tu-darmstadt.de>
2022-07-09 12:01:05 +0200

    Added 2 new configs (and deleted old ones)

--- model_configs.py
+++ model_configs.py
@@ -11,52 +11,16 @@ global_hyperparameters = {
 
 configs = [
     {
-        "training_mode": "growing",
-        "filter": "sobel",
-        "dataset": "BloodMNIST",
-        "image_index": 0,
-    },
-    {
-        "training_mode": "persistence",
-        "filter": "sobel",
-        "dataset": "BloodMNIST",
-        "image_index": 0,
-    },
-    {
-        "training_mode": "regeneration",
-        "filter": "sobel",
-        "dataset": "BloodMNIST",
-        "image_index": 0,
-    },
-    {
-        "training_mode": "regeneration",
-        "filter": "sobel",
-        "dataset": "RetinaMNIST",
-        "image_index": 0,
-    },
-    {
-        "training_mode": "regeneration",
-        "filter": "gauss_and_laplace",
-        "dataset": "BloodMNIST",
-        "image_index": 0,
-    },
-    {
-        "training_mode": "regeneration",
-        "filter": "fixed_random",
-        "dataset": "BloodMNIST",
-        "image_index": 0,
-    },
-    {
         "training_mode": "regeneration",
         "filter": "sobel",
         "dataset": "DermaMNIST",
-        "image_index": 4,
+        "image_index": 12,
     },
     {
         "training_mode": "regeneration",
         "filter": "sobel",
         "dataset": "PathMNIST",
-        "image_index": 6,
+        "image_index": 8,
     },
 ]
 
